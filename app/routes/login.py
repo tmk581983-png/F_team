102da0bff5b5ace55feb48a2e9d6d5a9535aaf28
behomazun <behomazun89@gmail.com>
2026-08-02 22:25:28 +0900

    ログイン画面の修正

--- app/routes/login.py
+++ app/routes/login.py
@@ -2,5 +2,5 @@ from flask import Blueprint, render_template
 login_bp = Blueprint("login", __name__, url_prefix="/login")
 
 @login_bp.route("/", methods=["GET"])
-def index():
+def login():
     return render_template("login.html")
