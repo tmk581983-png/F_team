b92c94fc768703c9b59e4d869bb353c584eccd14
behomazun <behomazun89@gmail.com>
2026-08-06 00:46:06 +0900

    login修正

--- app/app.py
+++ app/app.py
@@ -1,5 +1,5 @@
 from flask import Flask, redirect, session, url_for
-# from routes.login import login_bp
+from routes.login import login_bp
 from routes.signup import signup_bp
 from routes.mypage import mypage_bp
 from routes.room_list import room_list_bp
@@ -9,7 +9,7 @@ from routes.post import post_bp
 app = Flask(__name__)
 
 # Blueprint登録
-# app.register_blueprint(login_bp)
+app.register_blueprint(login_bp)
 app.register_blueprint(signup_bp)
 app.register_blueprint(mypage_bp)
 app.register_blueprint(room_list_bp)
--- app/routes/login.py
+++ app/routes/login.py
@@ -2,6 +2,7 @@ from flask import Blueprint, render_template
 login_bp = Blueprint("login", __name__, url_prefix="/login")
 
 @login_bp.route("/", methods=["GET"])
-def index():
+def login():
     return render_template("login.html")
+
 
