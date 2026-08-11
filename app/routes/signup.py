78e003d00a5a8365e3af60d723ec0123e064ea89
behomazun <behomazun89@gmail.com>
2026-08-11 15:12:33 +0900

    新規登録フォームに未入力チェックを追加

--- app/routes/signup.py
+++ app/routes/signup.py
@@ -20,14 +20,23 @@ def index():
         user_name = request.form["user_name"]
         password = request.form["password"]
 
+        #未入力の項目がないか確認する
+        if not login_id or not user_name or not password:
+            return render_template(
+            "signup.html",
+            error="すべての項目を入力してください"
+        )
+
     #受け取れた値をDockerのログで確認する
         print(f"Login ID: {login_id}")
         print(f"User Name: {user_name}")
         print(f"Password: {password}")
         print("新規登録フォームが送信されました")
 
+
     #登録ボタンを押したらマイページへ移動する
         return redirect(url_for("mypage.index"))
+    
 
-    #GETでもPOSTでも、最後に新規登録画面を表示する
+#GETでもPOSTでも、最後に新規登録画面を表示する
     return render_template("signup.html")
