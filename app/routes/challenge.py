9895c87ca0e446a3aad6554c9c48943cdf6177ae
kagaken <kagaken1192@gmail.com>
2026-08-16 23:56:59 +0900

    チャレンジルームに参加人数と参加者と最新投稿の表示追加

--- app/routes/challenge.py
+++ app/routes/challenge.py
@@ -1,4 +1,5 @@
 from flask import Flask,render_template,Blueprint
+from utils.db import get_connection
 
 challenge_bp = Blueprint("challenge",__name__, url_prefix="/challenge")
 
@@ -6,30 +7,66 @@ challenge_bp = Blueprint("challenge",__name__, url_prefix="/challenge")
 def room(room_id):
     if room_id == 1 :
       room_name="目指せ!150ステップ!"
-      return render_template("challenge_room.html",room_name=room_name)
 
     elif room_id == 2 :
        room_name="いくぜ!ステップ200~!!"
-       return render_template("challenge_room.html",room_name=room_name)
 
     elif room_id == 3 :
        room_name="ネットワークを極めたい!"
-       return render_template("challenge_room.html",room_name=room_name)
 
     elif room_id == 4 :
        room_name="Linux王に俺はなる!"
-       return render_template("challenge_room.html",room_name=room_name)
 
     elif room_id == 5 :
        room_name="言語化を制すものは!"
-       return render_template("challenge_room.html",room_name=room_name)
+
 
     elif room_id == 6 :
        room_name="休ませてくれ・・"
-       return render_template("challenge_room.html",room_name=room_name)
- 
+       
     else :
        return "Room not found", 404
+
+    conn = get_connection()
+    cursor = conn.cursor()
+
+   # 現在の参加者を探す→各参加者の最新日時を探す→その日時の投稿を特定
+    sql = """
+    SELECT
+    u.name,
+    p.contents
+    FROM room_participations AS rp
+
+    JOIN users AS u
+      ON rp.user_id = u.id
+
+    LEFT JOIN (
+      SELECT
+         user_id,
+         room_id,
+         MAX(created_at) AS latest_created_at
+      FROM posts
+      GROUP BY user_id, room_id
+    ) AS lp
+      ON rp.user_id = lp.user_id
+      AND rp.room_id = lp.room_id
+
+    LEFT JOIN posts AS p
+      ON rp.user_id = p.user_id
+      AND rp.room_id = p.room_id
+      AND p.created_at = lp.latest_created_at
+
+    WHERE rp.room_id = %s
+    AND rp.graduated_at IS NULL;
+    """
+    
+    cursor.execute(sql, (room_id,))
+    users = cursor.fetchall()
+    cursor.close()
+    conn.close()
+
+    return render_template("challenge_room.html",room_name=room_name, users=users)
+     
 
 @challenge_bp.route("/result")
 def result():
